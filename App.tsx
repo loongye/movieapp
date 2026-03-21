84aa191c75dee4467c849d2492b14b13caf1b659
yongen <493984+loongye@users.noreply.github.com>
2026-03-22 01:20:13 +0800

    feat: Integrate TanStack Query and create movie data hooks

--- App.tsx
+++ App.tsx
@@ -10,19 +10,25 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TMDB_API_READ_ACCESS_TOKEN } from './config';
 import { Page } from './components/home/Page';
+
+const queryClient = new QueryClient();
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppContent />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
+
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
---
+++ hooks/useMovieQueries.ts
@@ -0,0 +1,54 @@
+import { useQuery } from '@tanstack/react-query';
+import * as tmdbService from '../services/tmdbService';
+
+export const useNowPlayingMovies = (page = 1) => {
+  return useQuery({
+    queryKey: ['movies', 'now_playing', page],
+    queryFn: () => tmdbService.getNowPlayingMovies(page),
+  });
+};
+
+export const usePopularMovies = (page = 1) => {
+  return useQuery({
+    queryKey: ['movies', 'popular', page],
+    queryFn: () => tmdbService.getPopularMovies(page),
+  });
+};
+
+export const useUpcomingMovies = (page = 1) => {
+  return useQuery({
+    queryKey: ['movies', 'upcoming', page],
+    queryFn: () => tmdbService.getUpcomingMovies(page),
+  });
+};
+
+export const useMovieDetails = (movieId: number) => {
+  return useQuery({
+    queryKey: ['movie', movieId],
+    queryFn: () => tmdbService.getMovieDetails(movieId),
+    enabled: !!movieId,
+  });
+};
+
+export const useMovieCredits = (movieId: number) => {
+  return useQuery({
+    queryKey: ['movie', movieId, 'credits'],
+    queryFn: () => tmdbService.getMovieCredits(movieId),
+    enabled: !!movieId,
+  });
+};
+
+export const useAccountDetails = () => {
+  return useQuery({
+    queryKey: ['account'],
+    queryFn: () => tmdbService.getAccountDetails(),
+  });
+};
+
+export const useMovieRecommendations = (movieId: number, page = 1) => {
+  return useQuery({
+    queryKey: ['movie', movieId, 'recommendations', page],
+    queryFn: () => tmdbService.getMovieRecommendations(movieId, page),
+    enabled: !!movieId,
+  });
+};
